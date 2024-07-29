51dd4bbd8f0b5ee071f26d2d04005c3051c75bd0
ignacio <ign.garcia61@gmail.com>
2024-07-29 20:49:12 -0300

    feat: backend intial strucuture

---
+++ backend/src/main/java/com/garcia/splitpal/SplitpalApplication.java
@@ -0,0 +1,13 @@
+package com.garcia.splitpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitpalApplication.class, args);
+	}
+
+}
