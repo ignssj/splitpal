76afd96bf3e40272db1677a1796eb9f3468acabc
ignacio <ign.garcia61@gmail.com>
2024-07-30 18:16:46 -0300

    feat: create database tables

---
+++ backend/src/main/resources/db/migration/V1__initial-setup.sql
@@ -0,0 +1,42 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(15) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE split (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    category VARCHAR(15) NOT NULL,
+    total INTEGER NOT NULL,
+    qrcode VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE split_has_participants (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    organizer BOOLEAN DEFAULT false,
+    split_id UUID,
+    user_id UUID,
+    FOREIGN KEY (split_id) REFERENCES split(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE payments (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    receipt VARCHAR(255) NOT NULL,
+    value INTEGER NOT NULL,
+    split_id UUID,
+    user_id UUID,
+    FOREIGN KEY (split_id) REFERENCES split(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
