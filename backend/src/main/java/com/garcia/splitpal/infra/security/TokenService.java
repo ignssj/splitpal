b93c8022cc0ccdf66227c2d1b69524637136b26e
ignacio <ign.garcia61@gmail.com>
2024-08-01 10:17:53 -0300

    wip: add token service

--- backend/src/main/java/com/garcia/splitpal/infra/security/TokenService.java
+++ backend/src/main/java/com/garcia/splitpal/infra/security/TokenService.java
@@ -1,2 +1,53 @@
-package com.garcia.splitpal.infra.security;public class TokenService {
+package com.garcia.splitpal.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.garcia.splitpal.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+    public String generatedToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("splitpal-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+
+            return token;
+        }catch(JWTCreationException ex){
+            throw new RuntimeException("Error while creating JWT");
+        }
+    }
+
+    private String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("splitpal-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch(JWTVerificationException ex){
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-3"));
+    }
+
 }
