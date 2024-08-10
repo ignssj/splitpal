e0f37811587def281446cb9cd10a305eef1b1d58
ignacio <ign.garcia61@gmail.com>
2024-08-10 15:44:26 -0300

    feat: add show/hide password on userform

--- frontend/src/features/auth/components/UserForm/Pasword/index.tsx
+++ frontend/src/features/auth/components/UserForm/Pasword/index.tsx
@@ -2,9 +2,25 @@ import React from "react";
 import Input from "../../../../../components/Input";
 import styles from "../styles";
 import { IUserFormInput } from "../types";
+import { TextInput } from "react-native-paper";
 
 const UserPassword: React.FC<IUserFormInput> = ({ value, onChange }) => {
-  return <Input style={styles.input} label="Senha" value={value} onChangeText={onChange} />;
+  const [visible, setVisible] = React.useState<boolean>(false);
+
+  const togglePasswordVisibility = () => {
+    setVisible((prev) => !prev);
+  };
+
+  return (
+    <Input
+      style={styles.input}
+      label='Senha'
+      value={value}
+      onChangeText={onChange}
+      secureTextEntry={visible}
+      right={<TextInput.Icon icon='eye' size={24} onPress={togglePasswordVisibility} />}
+    />
+  );
 };
 
 export default UserPassword;
