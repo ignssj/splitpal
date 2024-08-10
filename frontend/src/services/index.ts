ae5d39455d049ccb8a686e7759f385ac201205e8
ignacio <ign.garcia61@gmail.com>
2024-08-09 23:07:40 -0300

    chore: setup development environment connection

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: __DEV__ ? "http://54.146.180.62:8080/api/" :  "http://localhost:8080/api/",
   timeout: 5000,
 });
 
