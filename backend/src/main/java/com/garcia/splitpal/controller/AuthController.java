741369c4aa6f1454982447eeab0b7c4f6856381d
ignacio <ign.garcia61@gmail.com>
2024-08-13 19:57:17 -0300

    change: add userId in authentication response

--- backend/src/main/java/com/garcia/splitpal/controller/AuthController.java
+++ backend/src/main/java/com/garcia/splitpal/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.garcia.splitpal.controller;
 
 import com.garcia.splitpal.dto.auth.AuthenticateUserDTO;
-import com.garcia.splitpal.dto.auth.AuthenticationResponseDTO;
 import com.garcia.splitpal.dto.user.CreateUserDTO;
 import com.garcia.splitpal.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
-@RequestMapping(value = "/api/auth", produces = {"application/json"})
+@RequestMapping(value = "/api/auth", produces = { "application/json" })
 @Tag(name = "Auth")
 public class AuthController {
 
@@ -22,21 +21,19 @@ public class AuthController {
 
     @Operation(summary = "Authenticates an user", description = "Returns a JWT if the user is authenticated")
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody AuthenticateUserDTO body){
-        var token = authService.authenticate(body);
-        if (token == null){
+    public ResponseEntity login(@RequestBody AuthenticateUserDTO body) {
+        var authenticatedUser = authService.authenticate(body);
+        if (authenticatedUser == null) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(new AuthenticationResponseDTO(body.username(), token));
+        return ResponseEntity.ok(authenticatedUser);
     }
 
     @Operation(summary = "Register an user", description = "Creates an user and returns its id")
     @PostMapping("/register")
-    public ResponseEntity register(@RequestBody CreateUserDTO body){
+    public ResponseEntity register(@RequestBody CreateUserDTO body) {
         var id = authService.register(body);
-        return ResponseEntity.created(URI.create("users/"+id.toString())).build();
+        return ResponseEntity.created(URI.create("users/" + id.toString())).build();
     }
-
-
 
 }
--- backend/src/main/java/com/garcia/splitpal/dto/auth/AuthenticationResponseDTO.java
+++
@@ -1,3 +0,0 @@
-package com.garcia.splitpal.dto.auth;
-
-public record AuthenticationResponseDTO(String username, String token){}
