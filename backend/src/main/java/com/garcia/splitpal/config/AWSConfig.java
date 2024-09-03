d254513b5db75a81bab2dbde8c44955986922331
ignacio <ign.garcia61@gmail.com>
2024-09-03 19:21:39 -0300

    fix: add default credentials to aws configuration file

--- backend/src/main/java/com/garcia/splitpal/config/AWSConfig.java
+++ backend/src/main/java/com/garcia/splitpal/config/AWSConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -17,6 +18,7 @@ public class AWSConfig {
     public AmazonS3 createS3Client() {
         return AmazonS3ClientBuilder
                 .standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
                 .withRegion(region)
                 .build();
     }
