2c6a24207f9cf5fa73224ee5fa861d41a6b6608f
ignacio <ign.garcia61@gmail.com>
2024-09-06 16:20:10 -0300

    fix: add background color to Screen.Loading

--- frontend/src/components/Screen/Loading/index.tsx
+++ frontend/src/components/Screen/Loading/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import styles from "./styles";
 import { ActivityIndicator } from "react-native-paper";
 import { IScreenLoading } from "../types";
+import View from "../../View";
 
 const ScreenLoading: React.FC<IScreenLoading> = ({ style }) => {
-  return <ActivityIndicator animating size={"large"} style={style || styles.activity} />;
+  return (
+    <View>
+      <ActivityIndicator animating size={"large"} style={style || styles.activity} />
+    </View>
+  );
 };
 
 export default ScreenLoading;
