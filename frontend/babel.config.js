65cebe791ad5fb2e80725dfbe2f094644849a052
ignacio <ign.garcia61@gmail.com>
2024-09-14 20:37:58 -0300

    chore: add ESM support to jest

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    presets: ['babel-preset-expo'],
-    plugins: [
-        ['@babel/plugin-transform-class-properties', { loose: true }],
-        ['@babel/plugin-transform-private-methods', { loose: true }],
-        ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-        ['react-native-paper/babel']
-    ]
+  presets: ["babel-preset-expo"],
+  plugins: [
+    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }],
+    ["react-native-paper/babel"],
+  ],
 };
